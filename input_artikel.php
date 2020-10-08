35c24c54e20f4117ca4bae1a926715a11c08fb9a
Mufliha Safira <muflihasafira@gmail.com>
2020-10-08 14:08:42 +0700

    a

--- input_artikel.php
+++ input_artikel.php
@@ -23,28 +23,28 @@
 	
 	<table width="100%" class="form">
 		<tr>
-			<td width="15%" margin-top="10px">Judul Artikel</td>
-			<td><input type="text" name="judul" size="40"></td>
+			<td width="15%" margin-top="10px">Judul Artikel :</td>
+			<td><input type="text" name="judul" size="40" style="padding: 6px; border-radius: 8px"></td>
 		</tr>
 		<tr>
-			<td width="15%">Nama Penulis</td>
-			<td><input type="text" name="nama" size="40"></td>
+			<td width="15%">Nama Penulis :</td>
+			<td><input type="text" name="nama" size="40" style="padding: 6px; border-radius: 8px"></td>
 		</tr>
 		<tr>
-			<td width="15%">Sinopsis</td>
-			<td><textarea name="sinopsis" rows="3" cols="90"></textarea></td>
+			<td width="15%">Sinopsis :</td>
+			<td><textarea style="padding: 10px; border-radius: 8px"name="sinopsis" rows="3" cols="90"></textarea></td>
 		</tr>
 		<tr>
-			<td>Tempat</td>
-			<td><input type="text" name="tempat" size="20"></td>
+			<td>Tempat : </td>
+			<td><input style="padding: 6px; border-radius: 8px" type="text" name="tempat" size="20"></td>
 		</tr>
 		<tr>
-			<td>Tanggal</td>
-			<td><input type="date" name="tanggal" size="20"></td>	
+			<td>Tanggal :</td>
+			<td><input style="padding: 6px; border-radius: 8px" type="date" name="tanggal" size="20"></td>	
 		</tr>
 		<tr>
-			<td>Isi Artikel</td>
-			<td><textarea name="isi" rows="20" cols="90"></textarea></td>
+			<td>Isi Artikel :</td>
+			<td><textarea style="padding: 20px; border-radius: 8px"name="isi" rows="20" cols="90"></textarea></td>
 		</tr>
 		<tr>
 			<td></td>
